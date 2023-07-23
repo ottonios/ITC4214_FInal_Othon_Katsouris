3d085d9cc1e2c015daff623f791193838b7af5ac
ottonios <97444392+ottonios@users.noreply.github.com>
2023-07-23 17:14:46 +0300

    Added comments

--- patisserie/website/forms.py
+++ patisserie/website/forms.py
@@ -2,18 +2,21 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile, Category, SubCategory, Item
 
+# Form for user registration
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-
+        
+# Form for updating user profile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['bio', 'profile_picture']
         
+# Form for filtering items based on various criteria        
 class ItemFilterForm(forms.Form):
     name = forms.CharField(max_length=100, required=False)
     min_price = forms.DecimalField(max_digits=10, decimal_places=2, required=False)
@@ -26,12 +29,15 @@ class ItemFilterForm(forms.Form):
         if 'category' in self.data:
             try:
                 category_id = int(self.data.get('category'))
+                # Filter sub-categories based on the selected category
                 self.fields['sub_category'].queryset = SubCategory.objects.filter(category_id=category_id)
             except (ValueError, TypeError):
                 pass
         elif self.is_bound:
+            # If form is bound but no category is selected, display no sub-categories
             self.fields['sub_category'].queryset = SubCategory.objects.none()
- 
+            
+# Form for adding or updating an item 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
